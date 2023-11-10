8470084a7a14d519f6f70a2e12f2d6b08e524762
Dan <134556824+botatana@users.noreply.github.com>
2023-11-10 17:00:50 +0300

    Add files via upload

---
+++ l2/index.php
@@ -0,0 +1,7 @@
+
+
+<?php
+$a = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+
+echo "<div style=\"font:bold 18px Arial; color:red; text-align:center;\">" . array_sum($a) . "</div>";
+?>
